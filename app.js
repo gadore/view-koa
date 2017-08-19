9e46746a50f1764a2288512e4e9d0f631b1b37ab
gadore <qianlizhihao@gmail.com>
2017-08-19 20:36:26 +0800

    push

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const isProduction = process.env.NODE_ENV === 'production';
-const bodyParser = require('koa-bodyparser');
 const Koa = require('koa');
+
+const bodyParser = require('koa-bodyparser');
+
 const controller = require('./controller');
+
 const templating = require('./templating');
 
 const app = new Koa();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+// log request URL:
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     var
@@ -16,25 +21,23 @@ app.use(async (ctx, next) => {
     ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
 
-
-
+// static file support:
 if (! isProduction) {
     let staticFiles = require('./static-files');
     app.use(staticFiles('/static/', __dirname + '/static'));
 }
 
+// parse request body:
 app.use(bodyParser());
 
-app.use(templating('view', {
+// add nunjucks as view:
+app.use(templating('views', {
     noCache: !isProduction,
-    watch: isProduction
+    watch: !isProduction
 }));
 
+// add controller:
 app.use(controller());
 
 app.listen(3000);
-console.log('App started at localhost:3000...');
-
-
-
-
+console.log('app started at port 3000...');
--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
-async (ctx, next) => {
-    ctx.render('index.html',{
-        title: 'Welcome'
-    });
-}
+module.exports = {
+    'GET /': async (ctx, next) => {
+        ctx.render('index.html', {
+            title: 'Welcome'
+        });
+    }
+};
